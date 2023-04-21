e6030d9586105e79dfd5a3a94350b891b85e6588
mnl187 <mnl187@gmail.com>
2023-04-21 06:26:25 +0200

    dododanie komponentów Login oraz jego wstepnego stylowania zz wykorzystaniem sass, cczyszczenie plików App.tsx, App.css, Index.css, #862jnm2tk

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,1 +1,25 @@
 import React from "react";
+import './Login.scss';
+
+export const Login = () => {
+    return (
+        <div className="page-background">
+            <div className="login-container">
+                <h1 className="logo">MegaK</h1>
+                <form>
+                    <input className="login-email" type="email" placeholder="E-mail"/>
+                    <input className="login-password" type="password" placeholder="Hasło"/>
+                    <p>
+                        <button type="button" className="link-button-password">Zapomniałeś hasła?</button>
+                    </p>
+                    <p>
+                        Nie masz konta? <button type="button" className="link-button-register">Zarejestruj się</button>
+                    </p>
+                    <button type="submit" className="login-button">
+                        Zaloguj się
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
