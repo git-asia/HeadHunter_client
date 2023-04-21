92fadde3ca7fffc402996d360ce8b1733a7d284a
mnl187 <mnl187@gmail.com>
2023-04-21 07:02:44 +0200

    dalsze stylowanie Login.tsx CU-862jnm2tk

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,16 +5,19 @@ export const Login = () => {
     return (
         <div className="page-background">
             <div className="login-container">
-                <h1 className="logo">MegaK</h1>
+                <img className="logo" src="../assets/logo" alt="MegaK"/>
                 <form>
                     <input className="login-email" type="email" placeholder="E-mail"/>
                     <input className="login-password" type="password" placeholder="Hasło"/>
-                    <p>
+                    <div>
                         <button type="button" className="link-button-password">Zapomniałeś hasła?</button>
-                    </p>
-                    <p>
-                        Nie masz konta? <button type="button" className="link-button-register">Zarejestruj się</button>
-                    </p>
+                    </div>
+                    <div className="link-register">
+                        <button type="button" className="link-register-button">
+                            <p>Nie masz konta?</p>
+                            Zarejestruj się
+                        </button>
+                    </div>
                     <button type="submit" className="login-button">
                         Zaloguj się
                     </button>
