cac1af898fe1f3d526467443332fe715e387c442
mnl187 <mnl187@gmail.com>
2023-04-21 20:49:29 +0200

    CU-862jnm2tk refaktoryzacja kodu na React-bootstrap, wstępna refaktoryzacja styli

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React from 'react';
+import {Button, Container, Form, Image, ButtonGroup, Row, Col} from 'react-bootstrap';
 import './Login.scss';
+import logoImage from '../assets/react.svg' // testowe logo
 
-export const Login = () => {
+export const Login: React.FC = () => {
     return (
         <div className="page-background">
-            <div className="login-container">
-                <img className="logo" src="../assets/logo" alt="MegaK"/>
-                <form>
-                    <input className="login-email" type="email" placeholder="E-mail"/>
-                    <input className="login-password" type="password" placeholder="Hasło"/>
-                    <div>
-                        <button type="button" className="link-button-password">Zapomniałeś hasła?</button>
-                    </div>
-                    <div className="link-register">
-                        <button type="button" className="link-register-button">
-                            <p>Nie masz konta?</p>
-                            Zarejestruj się
-                        </button>
-                    </div>
-                    <button type="submit" className="login-button">
-                        Zaloguj się
-                    </button>
-                </form>
-            </div>
+            <Container className="login-container">
+                <Row>
+                    <Col>
+                        <Form>
+                            <div className="logo">
+                                <Image src={logoImage} alt="MegaK Logo"/>
+                            </div>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control className="login-email" type="email" placeholder="E-mail"/>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control className="login-password" type="password" placeholder="Hasło"/>
+                            </Form.Group>
+                        </Form>
+                        <div className="forgot-password-container">
+                            <Button
+                                variant="link"
+                                className="forgot-password-link float-end"
+                                //onClick={() => {
+                                // }}
+                            >
+                                Zapomniałeś hasła?
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ButtonGroup className="mb-3">
+                            <Button className="forgot-password-link float-end" variant="outline-danger">Nie masz
+                                konta?
+                                Zarejestruj się</Button>
+                        </ButtonGroup>
+                    </Col>
+                    <Col>
+                        <ButtonGroup className="mb-3 float-end">
+                            <Button className="link-login-button" variant="danger">Zaloguj się</Button>
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+            </Container>
         </div>
-    )
-}
+    );
+};
