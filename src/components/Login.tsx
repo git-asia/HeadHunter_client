da62154257277ad3446f61d24aa34bba8d9b4f31
mnl187 <mnl187@gmail.com>
2023-04-28 07:02:05 +0200

    refaktoryzacja kodu na Material UI

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,52 +1,51 @@
 import React from 'react';
-import {Button, Container, Form, ButtonGroup, Row, Col} from 'react-bootstrap';
-import '../App.scss'
+import { Container, Grid, TextField, Button, Typography } from '@mui/material';
+import '../App.scss';
 import './Login.scss';
-
 
 export const Login: React.FC = () => {
     return (
         <div className="page-background">
-            <Container className="login-container">
-                <Row>
-                    <Col>
-                        <Form>
-                            <div className="logo"></div>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Control className="login-email" type="email" placeholder="E-mail"/>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Control className="login-password" type="password" placeholder="Hasło"/>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                </Row>
-                <Row className="justify-content-end">
-                    <Col xs={4}>
-                        <Button
-                            variant="light"
-                            className="forgot-password-link"
-                            //onClick={() => {
-                            // }}
-                        >
+            <Container maxWidth="md" className="login-container">
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <div className="logo"/>
+                        <TextField
+                            className="login-email"
+                            type="email"
+                            label="E-mail"
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </Grid>
+                        <Grid item xs={12}>
+                        <TextField
+                            className="login-password"
+                            type="password"
+                            label="Hasło"
+                            variant="outlined"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item container xs={12} justifyContent="flex-end">
+                        <Button className="forgot-password-link" color="primary">
                             Zapomniałeś hasła?
                         </Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xl={8}>
-                        <ButtonGroup className="mb-3">
-                            <p>Nie masz konta?</p>
-                            <Button className="register-link" variant="link">
-                                Zarejestruj się</Button>
-                        </ButtonGroup>
-                    </Col>
-                    <Col xl={4}>
-                        <ButtonGroup className="mb-3">
-                            <Button className="link-login-button" variant="light">Zaloguj się</Button>
-                        </ButtonGroup>
-                    </Col>
-                </Row>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Typography>Nie masz konta?</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button className="register-link" color="primary">
+                            Zarejestruj się
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button className="link-login-button" variant="contained">
+                            Zaloguj się
+                        </Button>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
