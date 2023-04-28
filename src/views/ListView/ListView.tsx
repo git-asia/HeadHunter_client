5389b336ef04eaaeffbdbec9d230a8e2a2878485
git-asia <tuszynska.joanna@gmail.com>
2023-04-28 15:26:45 +0200

    Added ListView.

---
+++ src/views/ListView/ListView.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { UserData } from "../../components/UserData/UserData";
+
+import "./ListView.scss";
+import "../../_variables.sass";
+
+
+export const ListView = () => {
+  return (
+    <>
+      <div className="page-wrapper">
+        <div className="list-wrapper">
+          <UserData />
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+
