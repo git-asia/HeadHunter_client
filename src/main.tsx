6cb7724d810ff19ccb57b96e59c492286c4eb5cd
git-asia <tuszynska.joanna@gmail.com>
2023-04-28 15:27:23 +0200

    Removed commented bootstrap-icons import.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 
-// import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
