dca706faa09eb1298526ea3dc0e7c37d1e235fc3
radziszop <radek.krochmall@gmail.com>
2023-04-30 00:21:16 +0200

    fixed invalid styles of UserData compponent

--- src/views/ListView/ListView.tsx
+++ src/views/ListView/ListView.tsx
@@ -5,18 +5,12 @@ import { UserData } from "../../components/UserData/UserData";
 import "./ListView.scss";
 import "../../_variables.sass";
 
-
 export const ListView = () => {
   return (
     <>
       <div className="page-wrapper">
-        <div className="list-wrapper">
-          <UserData />
-        </div>
+        <div className="list-wrapper">{/* <UserData /> */}</div>
       </div>
     </>
   );
 };
-
-
-
