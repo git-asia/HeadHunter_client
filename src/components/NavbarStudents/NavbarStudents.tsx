e1385a8b87640d5fa9bd94a3261c1a87c3f5958c
git-asia <tuszynska.joanna@gmail.com>
2023-04-29 12:31:33 +0200

    Created and styled element NavbarStudents

--- src/components/NavbarStudents/NavbarStudents.tsx
+++ src/components/NavbarStudents/NavbarStudents.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from 'react';
 
 import "./NavbarStudents.scss"
 
 export const NavbarStudents = () => {
+
+  const [activeCategory, setActiveCategory] = useState('allStudents');
+
+  const handleClick = (category) => {
+    setActiveCategory(category);
+  };
+
+  const handleKeyPress = (event, category) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      setActiveCategory(category);
+    }
+  };
+
   return (
     <div className="navbar-wrapper">
       <ul className="students-availability">
-        <li className="availability-option">
-          <p>Dostępni kursanci</p>
+        <li>
+          <button className={activeCategory === 'allStudents' ? 'active' : ''}
+            onClick={() => handleClick('allStudents')}
+            onKeyDown={(event) => handleKeyPress(event, 'allStudents')}
+             tabIndex="0"
+             aria-pressed={activeCategory === 'allStudents'}
+             type="button"
+          >
+            Dostępni kursanci
+          </button>
         </li>
-        <li className="availability-option">
-          <p>Do rozmowy</p>
+        <li>
+          <button className={activeCategory === 'forInterview' ? 'active' : ''}
+            onClick={() => handleClick('forInterview')}
+            onKeyDown={(event) => handleKeyPress(event, 'forInterview')}
+            tabIndex="0"
+            aria-pressed={activeCategory === 'forInterview'}
+            type="button"
+          >
+            Do rozmowy
+          </button>
         </li>
       </ul>
     </div>
