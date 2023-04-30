69bae310e2cb95d39fbb2464c77c2611676a04ca
kujawapatryk <homik363@wp.pl>
2023-04-30 06:38:03 +0200

    Add StyledEngineProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { StyledEngineProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
@@ -6,6 +7,8 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
   </React.StrictMode>,
 )
