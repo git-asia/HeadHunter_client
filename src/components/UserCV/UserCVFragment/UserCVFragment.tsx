8cb735c4511d5fe7052d64473729cf10aa0d5644
radziszop <radek.krochmall@gmail.com>
2023-05-01 22:20:30 +0200

    added UserCV component

---
+++ src/components/UserCV/UserCVFragment/UserCVFragment.tsx
@@ -0,0 +1,15 @@
+import "./UserCVFragment.scss";
+interface Props {
+  header: string;
+  children?: JSX.Element | JSX.Element[];
+}
+export const UserCVFragment = ({ header, children }: Props) => {
+  return (
+    <div className="UserCV__fragment">
+      <div className="UserCV__fragment__heading">
+        <h3>{header}</h3>
+      </div>
+      <div className="UserCV__fragment__value">{children}</div>
+    </div>
+  );
+};
