b8830fbfc0e86751027fadae3b6f1b1ac7b4d61e
mic-dem <michal.demski@interia.pl>
2023-05-01 22:16:53 +0200

    Added PasswordReminder

---
+++ src/components/PasswordReminder/PasswordReminder.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Grid, TextField, Button } from "@mui/material";
+import '../../App.scss';
+import './PasswordReminder.scss';
+
+export const PasswordReminder: React.FC = () => {
+  return (
+    <div className="page-background">
+      <Container maxWidth="md" className="login-container">
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <div className="logo"/>
+            <TextField
+              className="login-email"
+              color="primary"
+              type="email"
+              label="E-mail"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button className="link-login-button" variant="contained">
+              Zresetuj hasło
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
