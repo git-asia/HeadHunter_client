a57ac8da2365717efbbef16469e928b2c4be15fa
kujawapatryk <homik363@wp.pl>
2023-05-02 02:44:21 +0200

    Removing unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Header } from "./components/Header/Header";
 import { ListView } from "./views/ListView/ListView";
 
 import "./index.scss"
-import {Pagination} from "./components/Pagination/Pagination";
-
 
 export const App = () => {
 
