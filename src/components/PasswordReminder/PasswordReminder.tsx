eb59dbb58ff48c57f7fd6b3cab3670f1d878ae9a
git-asia <tuszynska.joanna@gmail.com>
2023-05-04 19:12:36 +0200

    Adjusted styles.

---
+++ src/components/PasswordReminder/PasswordReminder.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, Container, Grid, TextField } from "@mui/material";
+import logo from "../../assets/images/logo.png";
+import "../../App.scss";
+import "./PasswordReminder.scss";
+
+export const PasswordReminder: React.FC = () => {
+  return (
+    <div className="page-background">
+      <Container maxWidth="md" className="login-container">
+        <Grid container spacing={3}>
+          <Grid item xs={12} className="email-box">
+            <img src={logo} alt="Logo" className="logo" />
+            <TextField
+              className="login-email"
+              id="login-email"
+              color="primary"
+              type="email"
+              placeholder="E-mail"
+              variant="outlined"
+              fullWidth
+            />
+            <Grid container xs={12} justifyContent="center">
+              <Button className="login-btn">
+                Zresetuj hasło
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
