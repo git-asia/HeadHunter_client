02825afbca2ef9ad6ae78a5116a101046cb8ceb0
mnl187 <mnl187@gmail.com>
2023-05-06 12:17:17 +0200

    Adding 'Outlet' to render the appropriate nested routes if the user is logged in, otherwise they are redirected to the login page '/'

--- src/components/Auth/AuthWrapper.tsx
+++ src/components/Auth/AuthWrapper.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Navigate, Outlet } from "react-router-dom";
 
 interface AuthWrapperProps {
   isLoggedIn: boolean;
-};
+}
 
 export const AuthWrapper: React.FC<AuthWrapperProps> = (isLoggedIn) => {
-  return isLoggedIn
+  return isLoggedIn ? <Outlet/> : <Navigate to="/"/>;
 };
