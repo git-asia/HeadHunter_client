86cd6a9016245b06e87cb3922ae581ed49c159d2
mnl187 <mnl187@gmail.com>
2023-05-06 13:48:10 +0200

    Adding AuthWrapper to App.js file, using hardcoded isLoggedIn=true value, fixing the error related to routing in Outlet and using the children property

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import { CVView } from "./views/CVView/CVView";
 import { Login } from "./components/Login/Login";
 
 import "./index.scss"
+import { AuthWrapper } from "./components/Auth/AuthWrapper";
 
 
 export const App = () => {
+  const isLoggedIn = true; //do zmiany w momencie przekazywania wartości z backend
 
   return (
     <>
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/list"
-               element={<ListView/>}/>
-        <Route path="/cv" element={<CVView/>}/>
+               element={<AuthWrapper isLoggedIn={isLoggedIn}><ListView/></AuthWrapper>}/>
+        <Route path="/cv"
+               element={<AuthWrapper isLoggedIn={isLoggedIn}><CVView/></AuthWrapper>}/>
       </Routes>
     </>
   );
--- src/components/Auth/AuthWrapper.tsx
+++ src/components/Auth/AuthWrapper.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 interface AuthWrapperProps {
   isLoggedIn: boolean;
+  children: React.ReactNode
 }
 
-export const AuthWrapper: React.FC<AuthWrapperProps> = (isLoggedIn) => {
-  return isLoggedIn ? <Outlet/> : <Navigate to="/"/>;
+export const AuthWrapper: React.FC<AuthWrapperProps> = ({ isLoggedIn, children }) => {
+  return isLoggedIn ? <>{children}</> : <Navigate to="/" />;
 };
