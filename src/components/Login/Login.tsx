d8359ea2279ae97da5b3cf94e411a0fd2e854016
mnl187 <mnl187@gmail.com>
2023-05-06 15:24:38 +0200

    Adding initial configuration in Login.tsx for the login button, adding useNavigate and useState hooks responsible for storing user states and navigation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Grid, TextField } from "@mui/material";
 import logo from "../../assets/images/logo.png";
 import "../../App.scss";
 import "./Login.scss";
+import { useNavigate } from "react-router-dom";
 
 export const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
+  const handeSubmit = async (e: React.MouseEvent) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="page-background">
       <Container className="login-container">
-        <Grid container spacing={3}>
+        <Grid container spacing={1}>
           <Grid item xs={12} className="email-box">
             <img src={logo} alt="Logo" className="logo" />
             <TextField
@@ -18,6 +27,7 @@ export const Login: React.FC = () => {
               placeholder="E-mail"
               variant="outlined"
               fullWidth
+              value={email}
             />
             <TextField
               className="login-pass"
@@ -26,6 +36,7 @@ export const Login: React.FC = () => {
               placeholder="Hasło"
               variant="outlined"
               fullWidth
+              value={password}
             />
           </Grid>
           <Grid item xs={12} container justifyContent="flex-end">
@@ -48,7 +59,7 @@ export const Login: React.FC = () => {
               </Button>
             </Grid>
             <Grid item>
-              <Button className="login-btn">
+              <Button className="login-btn" onClick={handeSubmit}>
                 Zaloguj się
               </Button>
             </Grid>
