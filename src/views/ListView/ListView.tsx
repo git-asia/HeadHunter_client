85e3fc9009c4734b3352f08678741e7f4350722f
kujawapatryk <homik363@wp.pl>
2023-05-07 16:35:55 +0200

    Removing button corner rounding

--- src/views/ListView/ListView.tsx
+++ src/views/ListView/ListView.tsx
@@ -4,11 +4,13 @@ import {Pagination} from "../../components/Pagination/Pagination";
 
 import "./ListView.scss";
 import "../../_variables.sass";
+import {SearchFilterBar} from "../../components/SearchFilterBar/SearchFilterBar";
 
 export const ListView = () => {
   return (
     <>
       <div className="page-wrapper">
+          <SearchFilterBar/>
         <div className="list-wrapper">{/* <UserData /> */}</div>
         <Pagination/>
       </div>
