d3ba9f498fbc08e92763647ce381104d4794e734
kujawapatryk <homik363@wp.pl>
2023-05-11 02:17:38 +0200

    creation of 3 contexts for components

---
+++ src/contexts/filter.context.ts
@@ -0,0 +1,29 @@
+import {createContext} from "react";
+
+const initialState = {
+    expectedTypeWork: {
+        remoteWork: false,
+        inOffice: false,
+    },
+    expectedContractType: {
+        employmentContract: false,
+        b2b: false,
+        mandateContract: false,
+        workContract: false,
+    },
+    expectedSalary: {
+        min: "",
+        max: "",
+    },
+    canTakeApprenticeship: null,
+    monthsOfCommercialExp: null,
+    courseCompletion: 1,
+    courseEngagement: 1,
+    projectDegree: 1,
+    teamProjectDegree: 1,
+};
+
+export const FilterContext = createContext({
+    filterCon: initialState,
+    setFilterCon: (s: any) => {s},
+})
