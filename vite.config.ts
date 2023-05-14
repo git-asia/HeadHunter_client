5def571ccdc8a9e7e8fe44b9ba65586767b1b0a7
Joanna <tuszynska.joanna@gmail.com>
2023-05-14 22:21:41 +0200

    Configured plugin for .env

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,19 @@ import eslint from "vite-plugin-eslint";
 import sassDts from 'vite-plugin-sass-dts'
 
 // https://vitejs.dev/config/
+const { createEnvCompatiblePlugin } = require('vite-plugin-env-compatible');
+
 export default defineConfig({
-  plugins: [react(), eslint(), sassDts()],
+  plugins: [
+    react(),
+    eslint(),
+    sassDts(),
+    createEnvCompatiblePlugin({
+      mappings: {
+        VITE_API_URL: 'process.env.VITE_API_URL',
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
   },
