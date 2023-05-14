7fa860c9071463e71e66c62d2e4a454689740d23
mic-dem <michal.demski@interia.pl>
2023-05-14 17:47:12 +0200

    Added validation

--- src/components/AddHr/AddHr.tsx
+++ src/components/AddHr/AddHr.tsx
@@ -12,6 +12,11 @@ export const AddHr: React.FC = () => {
         maxReservedStudents: "",
     };
     const [form, setForm] = useState(emptyForm);
+    const [validForm1, setValidForm1] = useState(false);
+    const [validForm2, setValidForm2] = useState(false);
+    const [validForm3, setValidForm3] = useState(false);
+    const [validForm4, setValidForm4] = useState(false);
+
 
     const updateForm = (key: string, value: string|number) => {
         setForm(form => ({
@@ -20,8 +25,18 @@ export const AddHr: React.FC = () => {
         }))
     };
 
+
     const sendForm = async (e: SyntheticEvent) => {
         e.preventDefault();
+        setValidForm1(!form.email.includes('@'));
+        setValidForm2(form.fullName==='');
+        setValidForm3(form.company==='');
+        setValidForm4(Number(form.maxReservedStudents)<1 || Number(form.maxReservedStudents)>999);
+
+        if ((!form.email.includes('@')) || (form.fullName==='') || (form.company==='') || (Number(form.maxReservedStudents)<1 || Number(form.maxReservedStudents)>999)) {
+            console.log("Popraw dane w formularzu")
+        }else {
+
         try {
             const res = await fetch(`http://localhost:3001/manage/add-hr/:${form.email}/:${form.fullName}/:${form.company}/:${form.maxReservedStudents}'`);
             const data = await res.json();
@@ -33,6 +48,7 @@ export const AddHr: React.FC = () => {
         catch (e) {
             console.log("Coś poszło nie tak. Spróbuj później.")
         }
+        }
     };
 
 
@@ -43,44 +59,65 @@ export const AddHr: React.FC = () => {
                     <Grid item xs={12}>
                         <img src={logo} alt="Logo" className="logo" />
                         <Grid>
+                            <p className="info-validation"
+                            style= {{ display: validForm1 ? "" : "none" }}
+                            >Podaj prawidłowy adres e-mail</p>
                             <TextField
                                 type="email"
                                 placeholder="E-mail"
                                 value={form.email}
                                 onChange={e => {
                                     updateForm('email', e.target.value);
-
+                                    setValidForm1(!form.email.includes('@'));
                                 }}
                                 variant="outlined"
                                 fullWidth
                             />
                         </Grid>
                         <Grid>
+                            <p className="info-validation"
+                               style= {{ display: validForm2 ? "" : "none" }}
+                            >Podaj imię i nazwisko HR</p>
                             <TextField
                                 type="text"
                                 placeholder="Imię i nazwisko"
                                 value={form.fullName}
-                                onChange={e => updateForm('fullName', e.target.value)}
+                                onChange={e => {
+                                    updateForm('fullName', e.target.value);
+                                    setValidForm2(form.fullName==='');
+                                }}
                                 variant="outlined"
                                 fullWidth
                             />
                         </Grid>
                         <Grid>
+                            <p className="info-validation"
+                               style= {{ display: validForm3 ? "" : "none" }}
+                            >Podaj nazwę firmy HR</p>
                             <TextField
                                 type="text"
                                 placeholder="Firma"
                                 value={form.company}
-                                onChange={e => updateForm('company', e.target.value)}
+                                onChange={e => {
+                                    updateForm('company', e.target.value);
+                                    setValidForm3(form.company==='');
+                                }}
                                 variant="outlined"
                                 fullWidth
                             />
                         </Grid>
                         <Grid>
+                            <p className="info-validation"
+                               style= {{ display: validForm4 ? "" : "none" }}
+                            >Podaj liczbę między 1-999</p>
                             <TextField
                                 type="number"
                                 placeholder="Max. liczba kursantów"
                                 value={form.maxReservedStudents}
-                                onChange={e => updateForm('maxReservedStudents', e.target.value)}
+                                onChange={e => {
+                                    updateForm('maxReservedStudents', e.target.value);
+                                    setValidForm4(Number(form.maxReservedStudents)<1 || Number(form.maxReservedStudents)>999);
+                                }}
                                 variant="outlined"
                                 fullWidth
                             />
