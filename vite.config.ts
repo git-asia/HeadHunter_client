495a5eab88748dc1ce27c88be00f429cbf1277a1
Joanna <tuszynska.joanna@gmail.com>
2023-05-14 22:37:53 +0200

    Commented plugin conf. Not working with ts. Not sure if is needed at all.

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import eslint from "vite-plugin-eslint";
 import sassDts from 'vite-plugin-sass-dts'
 
 // https://vitejs.dev/config/
-const { createEnvCompatiblePlugin } = require('vite-plugin-env-compatible');
+//const { createEnvCompatiblePlugin } = require('vite-plugin-env-compatible');
 
 export default defineConfig({
   plugins: [
     react(),
     eslint(),
     sassDts(),
-    createEnvCompatiblePlugin({
-      mappings: {
-        VITE_API_URL: 'process.env.VITE_API_URL',
-      },
-    }),
+    // createEnvCompatiblePlugin({
+    //   mappings: {
+    //     VITE_API_URL: 'process.env.VITE_API_URL',
+    //   },
+    // }),
   ],
   build: {
     outDir: 'dist',
