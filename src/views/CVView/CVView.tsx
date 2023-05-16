963e948dbe032d8295ca2ab3f2bd79377df27c52
mic-dem <michal.demski@interia.pl>
2023-05-16 21:55:03 +0200

    Added match with BE

--- src/views/CVView/CVView.tsx
+++ src/views/CVView/CVView.tsx
@@ -2,12 +2,74 @@ import { UserCV } from "../../components/UserCV/UserCV";
 import { UserCard } from "../../components/UserCard/UserCard";
 import { IoIosArrowDown } from "react-icons/io";
 import { Header } from "../../components/Header/Header";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./CVView.scss";
 import "../../index.scss"
 
+interface Props {
+    bio: string;
+    bonusProjectUrls: string;
+    canTakeApprenticeship: number;
+    courseCompletion: number;
+    courseEngagement: number;
+    courses: string;
+    education: string;
+    expectedContractType: number;
+    expectedSalary: number;
+    expectedTypeWork: number;
+    firstName: string;
+    githubUsername: string;
+    lastName: string;
+    monthsOfCommercialExp: number;
+    phoneNumber: string;
+    portfolioUrls: string[];
+    projectDegree: number;
+    projectUrls: string[];
+    targetWorkCity: string;
+    teamProjectDegree: number;
+    userStatus: number;
+    workExperience: string
+}
+
 export const CVView = () => {
+    const id = "46f84261-df9d-11ed-a2b7-24fd5235b3db";
+    const [data, setData] = useState({
+        bio:"",
+        bonusProjectUrls : "",
+        canTakeApprenticeship : 1,
+        courseCompletion : 1,
+        courseEngagement : 1,
+        courses : "",
+        education : "",
+        expectedContractType : 1,
+        expectedSalary : 0,
+        expectedTypeWork : 1,
+        firstName : "",
+        githubUsername : "",
+        lastName : "",
+        monthsOfCommercialExp : 0,
+        phoneNumber : "",
+        portfolioUrls : [],
+        projectDegree : 1,
+        projectUrls : [],
+        targetWorkCity : "",
+        teamProjectDegree : 1,
+        userStatus : 1,
+        workExperience : "",
+    });
+
+    useEffect( () => {
+        const fetchData = async () => {
+            const res = await fetch(`http://localhost:3001/student/getcv/${id}`);
+            const downloadData = (await res.json())[0];
+            setData(downloadData);
+            console.log(data);
+        }
+        fetchData()
+            .catch(console.error);
+    }, []);
+
   return (
     <div className="CVView__container">
       <Header/>
@@ -17,38 +79,30 @@ export const CVView = () => {
           <span className="CVView__back__span">Wróć</span>
         </div>
         <UserCard
-          aboutMe={
-            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea ratione esse nostrum eveniet distinctio facilis cumque, iusto eius unde? Velit suscipit dolorem excepturi magnam ut incidunt aspernatur expedita, doloremque esse."
-          }
-          name="Imię Nazwisko"
-          github="github"
+          aboutMe={data.bio}
+          name={(data.firstName) + " " + (data.lastName)}
+          github={data.githubUsername}
           email="email@gmail.com"
-          phoneNumber="+48123456789"
+          phoneNumber={data.phoneNumber}
         />
         <UserCV
           grades={[
-            { header: "Ocena projektu kursu", value: "3/5" },
-            { header: "Ocena aktywności i  zaangażowania na kursie", value: "5/5" },
-            { header: "Ocena kodu w projekcie własnym", value: "5/5" },
-            { header: "Ocena pracy w zespole w Scrum", value: "4/5" },
+            { header: "Ocena projektu kursu", value: `${data.courseCompletion}/5` },
+            { header: "Ocena aktywności i  zaangażowania na kursie", value: `${data.courseEngagement}/5` },
+            { header: "Ocena kodu w projekcie własnym", value: `${data.teamProjectDegree}/5` },
+            { header: "Ocena pracy w zespole w Scrum", value: `${data.projectDegree}/5` },
           ]}
           expectations={[
-            { header: "Preferowane miejsce pracy", value: "Biuro" },
-            { header: "Docelowe miasto, gdzie chce pracować kandydat", value: "Warszawa" },
-            { header: "Oczekiwany typ kontraktu", value: "Umowa o pracę" },
-            { header: "Oczekiwane wynagrodzenie miesięczne netto", value: "8000 zł" },
-            { header: "Zgoda na odbycie bezpłatnych praktyk/stażu na początek", value: "Tak" },
-            { header: "Komercyjne doświadczenie w programowaniu", value: "6 Miesięcy" },
+            { header: "Preferowane miejsce pracy", value: `${data.expectedTypeWork}` },
+            { header: "Docelowe miasto, gdzie chce pracować kandydat", value: `${data.targetWorkCity}` },
+            { header: "Oczekiwany typ kontraktu", value: `${data.expectedContractType}` },
+            { header: "Oczekiwane wynagrodzenie miesięczne netto", value: `${data.expectedSalary} zł` },
+            { header: "Zgoda na odbycie bezpłatnych praktyk/stażu na początek", value: `${data.canTakeApprenticeship}` },
+            { header: "Komercyjne doświadczenie w programowaniu", value: `${data.monthsOfCommercialExp} miesięcy` },
           ]}
-          education={
-            " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat sit amet ex in tincidunt. Proin eget magna urna. Vivamus vitae velit nec massa pharetra tincidunt. Nullam ornare fringilla nulla in faucibus. Donec accumsan venenatis magna id aliquet. Integer a rutrum lectus. Ut feugiat tincidunt convallis. "
-          }
-          courses={
-            " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat sit amet ex in tincidunt. Proin eget magna urna. Vivamus vitae velit nec massa pharetra tincidunt. Nullam ornare fringilla nulla in faucibus. Donec accumsan venenatis magna id aliquet. Integer a rutrum lectus. Ut feugiat tincidunt convallis. "
-          }
-          experience={
-            " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat sit amet ex in tincidunt. Proin eget magna urna. Vivamus vitae velit nec massa pharetra tincidunt. Nullam ornare fringilla nulla in faucibus. Donec accumsan venenatis magna id aliquet. Integer a rutrum lectus. Ut feugiat tincidunt convallis. "
-          }
+          education={data.education}
+          courses={data.courses}
+          experience={data.workExperience}
           portfolio={["https://test1.pl", "https://test3.pl", "https://test6.pl"]}
           finalProjects={["https://test1.pl", "https://test3.pl", "https://test7.pl"]}
           scramProjects={["https://test2.pl", "https://test4.pl", "https://test8.pl"]}
