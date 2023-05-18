4be668c41a63f448b4f4e2d36ee1de81814c9b78
Joanna <tuszynska.joanna@gmail.com>
2023-05-18 21:07:03 +0200

    Added enums on fe.

--- src/components/UserData/UserData.tsx
+++ src/components/UserData/UserData.tsx
@@ -8,6 +8,23 @@ import { PaginationContext } from "../../contexts/pagination.context";
 
 import "./UserData.scss";
 
+enum TypeWork {
+  "Praca w biurze" = 1,
+  "Praca zdalna" = 2,
+}
+
+enum ContractType {
+  "Umowa o pracę" = 1,
+  "B2B" = 2,
+  "Umowa zlecenie" = 3,
+  "Umowa o dzieło" = 4,
+}
+
+enum Internship {
+  "Nie",
+  "Tak",
+}
+
 interface Props {
   id: string;
   name: string;
@@ -98,13 +115,13 @@ export const UserData = () => {
           { header: "Ocena aktywności i zaangażowania na kursie", value: item.courseEngagement + "/5" },
           { header: "Ocena kodu w projekcie własnym", value: item.projectDegree + "/5" },
           { header: "Ocena pracy w zespole w Scrum", value: item.teamProjectDegree + "/5" },
-          { header: "Preferowane miejsce pracy", value: [item.expectedTypeWork] },
+          { header: "Preferowane miejsce pracy", value: TypeWork[item.expectedTypeWork] },
           { header: "Docelowe miasto, gdzie chce pracować kandydat", value: item.targetWorkCity },
-          { header: "Oczekiwany typ kontraktu", value: [item.expectedContractType] },
+          { header: "Oczekiwany typ kontraktu", value: ContractType[item.expectedContractType] },
           { header: "Oczekiwane wynagrodzenie miesięczne netto", value: item.expectedSalary + " zł" },
           {
             header: "Zgoda na odbycie bezpłatnych praktyk/stażu na początek",
-            value: [item.canTakeApprenticeship],
+            value: Internship[item.canTakeApprenticeship],
           },
           { header: "Komercyjne doświadczenie w programowaniu", value: item.monthsOfCommercialExp.toString() },
         ],
