de98baaf8ab9bf7254ae23f176d8eba8517c9c33
kujawapatryk <homik363@wp.pl>
2023-05-26 04:27:59 +0200

    Adding support for types from the backend.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import sassDts from 'vite-plugin-sass-dts'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 //const { createEnvCompatiblePlugin } = require('vite-plugin-env-compatible');
@@ -11,6 +12,7 @@ export default defineConfig({
         react(),
         eslint(),
         sassDts(),
+        tsconfigPaths(),
     // createEnvCompatiblePlugin({
     //   mappings: {
     //     VITE_API_URL: 'process.env.VITE_API_URL',
